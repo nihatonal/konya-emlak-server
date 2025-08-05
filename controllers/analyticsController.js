3b5430394220c0d144c7cea6559f441d9ef63b84
nihatonal <onalnihat1986@gmail.com>
2025-08-05 13:32:07 +0300

    fixed json files

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 import { BetaAnalyticsDataClient } from '@google-analytics/data';
 // Google Analytics hizmet hesabı anahtar dosyasının yolu
 const analyticsDataClient = new BetaAnalyticsDataClient({
-  keyFile: path.join(__dirname, '../config/GA_KEY.json'),
+  credentials: JSON.parse(process.env.GA_CREDENTIALS_JSON),
 });
 
 const GA_PROPERTY = 'properties/479914818';
